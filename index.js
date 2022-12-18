c2c0a72fd5a63a796addc473275e3d9b9acd7e8b
Galen Beck Gatzke <gbgatzke@gmail.com>
2022-12-18 16:38:41 -0700

    commit tree 876fa4b524d6c8fa2ec03715ea650293b537b81e

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // code your solution here
+const record = [
+    { year: "2015", 
+    result: "W"
+},
+    { year: "2014", 
+    result: "N/A"
+},
+    { year: "2013", 
+    result: "L"
+},
+  ]
+function superbowlWin(){
+    const result = record.find(function() {
+        if (record['result'] === "W") {
+            return record['year']
+        }else {
+            return "undefined"
+        }
+    });
+}
+
+
+    //return result ? result.year : 'undefined';
+
+
+
+
+//superbowlWin = (record) => {
+//   const result = record.find(record => record.result === 'W');
+//   return !!result ? result.year : "undefined"
+//    }
+
+//superbowlWin = (record) => {
+//    const result = record.find( record => record.result === "W" );
+//    console.log(result)
+//    return record.result === "W" ? record.year : 'undefined';
+//  }
+//
+//superbowlWin = (record) => {
+//    const result = record.find( record => record.result === "W" );
+//    return !!result ? result.year : 'undefined';
+//  }
+//
+
+//superbowlWin = (record) => {
+//    const result = record.find(() => record.result === 'W')
+//    return !!result ? result.year : undefined }
+
+
